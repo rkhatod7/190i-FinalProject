79630f3a91a800888ea3fd5a5910643ded7c467a
Radhika Khatod <Radhika@169-231-88-30.wireless.ucsb.edu>
2017-06-08 16:27:33 -0700

    can see prof pic

--- app/src/main/java/com/example/radhika/finalproject/MainActivity.java
+++ app/src/main/java/com/example/radhika/finalproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.radhika.finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +13,23 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
 
 public class MainActivity extends AppCompatActivity {
     private LoginButton loginButton;
+
+    private ProfileTracker mProfileTracker;
+
+
     CallbackManager callbackManager;
     Button continueButton;
-
+    ProfilePictureView profileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         continueButton = (Button) findViewById(R.id.continue_button);
+        profileImage = (ProfilePictureView) findViewById(R.id.profilePicture);
+
         loginButton.setReadPermissions("email");
         // If using in a fragment
         //loginButton.setFragment(this);
@@ -42,12 +52,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Profile profile = Profile.getCurrentProfile();
+        profileImage.setProfileId(profile.getId());
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
+                        if(Profile.getCurrentProfile() == null) {
+                            mProfileTracker = new ProfileTracker() {
+                                @Override
+                                protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                                    // profile2 is the new profile
+                                    Log.v("facebook - profile", profile2.getFirstName());
+                                    mProfileTracker.stopTracking();
+                                }
+                            };
+                            // no need to call startTracking() on mProfileTracker
+                            // because it is called by its constructor, internally.
+                        }
+                        else {
+                            Profile profile = Profile.getCurrentProfile();
+                            profileImage.setProfileId(profile.getId());
 
+                            Log.v("facebook - profile", profile.getFirstName());
+                        }
                         // App code
                     }
 
@@ -66,7 +95,26 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+                Log.d("RADHIKA", "in on success");
                 // App code
+                if(Profile.getCurrentProfile() == null) {
+                    mProfileTracker = new ProfileTracker() {
+                        @Override
+                        protected void onCurrentProfileChanged(Profile profile, Profile profile2) {
+                            // profile2 is the new profile
+                            Log.v("facebook - profile", profile2.getFirstName());
+                            mProfileTracker.stopTracking();
+                        }
+                    };
+                    // no need to call startTracking() on mProfileTracker
+                    // because it is called by its constructor, internally.
+                }
+                else {
+                    Profile profile = Profile.getCurrentProfile();
+                    profileImage.setProfileId(profile.getId());
+
+                    Log.v("facebook - profile", profile.getFirstName());
+                }
             }
 
             @Override
@@ -88,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
         Intent intent = new Intent(this, HomePage.class);
+
         startActivity(intent);
     }
+
 }
